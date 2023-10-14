11ac8ed1d70950817f4a7506c2353dbc4a0d0ce6
Sydney Macdonald <macdonaldpsydney@gmail.com>
2023-10-14 14:10:00 -0700

    Adding interface types

--- Medical-Professional-Perspective/medical-professional-perspective/src/my-availability/my-availability.ts
+++ Medical-Professional-Perspective/medical-professional-perspective/src/my-availability/my-availability.ts
@@ -1,3 +1,34 @@
 /**
  * For medical professionals to submit their availability to Appointments
  */
+
+/**
+ * Date: DD/MM/YYYY
+ * TODO: this should probably be moved to a more generic file
+ */
+interface Date {
+    day: number
+    month: number
+    year: number
+}
+
+/**
+ * Time: HH/MM in 24 hour time
+ * TODO: this should probably be moved to a more generic file
+ */
+interface Time {
+    hour: number
+    minute: number
+}
+
+/**
+ * Schedule
+ * Types TBD
+ */
+interface Schedule {
+    doctor: number
+    hoursStart: Time
+    hoursEnd: Time
+    days: String[]
+    holidays?: Date
+}
