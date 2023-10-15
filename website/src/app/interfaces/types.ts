ff772a478abeeaf05843c338ffe3856f32510157
Sydney Macdonald <macdonaldpsydney@gmail.com>
2023-10-15 16:33:15 -0700

    Clean up file

--- website/src/app/interfaces/types.ts
+++ website/src/app/interfaces/types.ts
@@ -2,21 +2,11 @@
  * Date: DD/MM/YYYY
  */
 export type Date = `${number}/${number}/${number}`
-// export interface Date {
-//     day: number
-//     month: number
-//     year: number
-// }
 
 /**
  * Time: HH:MM in 24 hour time
  */
 export type Time = `${number}:${number}`
-// export interface Time {
-//     hour: number
-//     minute: number
-// }
-
 
 /**
  * Schedule
