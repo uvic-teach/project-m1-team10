9560efdd930305e2569fe43e7279d04483e49695
Sydney Macdonald <macdonaldpsydney@gmail.com>
2023-10-15 16:31:10 -0700

    Update data types to match appointment service

--- website/src/app/interfaces/data.ts
+++ website/src/app/interfaces/data.ts
@@ -3,22 +3,22 @@ import { Date, Time } from "./types";
 const doctors = [
     {
         id: 1,
-        hoursStart: {hour: 9, minute: 0} as Time,
-        hoursEnd: {hour: 17, minute: 0} as Time,
+        hoursStart: "09:00" as Time,
+        hoursEnd: "17:00" as Time,
         days: ["Monday", "Tuesday", "Wednesday"],
         holidays: null
     },
     {
         id: 2,
-        hoursStart: {hour: 8, minute: 0} as Time,
-        hoursEnd: {hour: 15, minute: 0} as Time,
+        hoursStart: "08:00" as Time,
+        hoursEnd: "15:00" as Time,
         days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-        holidays: {day: 30, month: 9, year: 2023} as Date
+        holidays: "30/09/2023" as Date
     },
     {
         id: 3,
-        hoursStart: {hour: 10, minute: 0} as Time,
-        hoursEnd: {hour: 16, minute: 0} as Time,
+        hoursStart: "10:00" as Time,
+        hoursEnd: "16:30" as Time,
         days: ["Monday", "Tuesday", "Saturday"],
         holidays: null
     }
--- website/src/app/interfaces/types.ts
+++ website/src/app/interfaces/types.ts
@@ -1,41 +1,25 @@
 /**
- * For medical professionals to submit their availability to Appointments
- */
-
-/**
  * Date: DD/MM/YYYY
- * TODO: this should probably be moved to a more generic file
  */
-export interface Date {
-    day: number
-    month: number
-    year: number
-}
-
-/**
- * Time: HH/MM in 24 hour time
- * TODO: this should probably be moved to a more generic file
- */
-export interface Time {
-    hour: number
-    minute: number
-}
-
-/**
- * Days of the week
- */
-// export enum DaysOfTheWeek {
-//     Sunday,
-//     Monday,
-//     Tuesday,
-//     Thursday,
-//     Friday,
-//     Saturday
+export type Date = `${number}/${number}/${number}`
+// export interface Date {
+//     day: number
+//     month: number
+//     year: number
 // }
 
 /**
+ * Time: HH:MM in 24 hour time
+ */
+export type Time = `${number}:${number}`
+// export interface Time {
+//     hour: number
+//     minute: number
+// }
+
+
+/**
  * Schedule
- * Types TBD
  */
 export interface Schedule {
     doctor: number
