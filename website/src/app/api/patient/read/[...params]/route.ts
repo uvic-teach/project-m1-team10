59d6ce0521d779a1113b957b7845d144eac90a28
odavid <odavid@uvic.ca>
2023-11-05 14:14:33 -0800

    Added Singleton pattern to global prisma instance and setup availability pattern

--- website/src/app/api/patient/read/[...params]/route.ts
+++ website/src/app/api/patient/read/[...params]/route.ts
@@ -3,8 +3,15 @@ import { NextApiResponse, NextApiRequest, NextApiHandler} from "next";
 
 export async function GET(req:NextApiRequest, context: { params:any }) {
     try{
-        const allAvailabilty = await prisma.availability.findMany()
-        return Response.json({user: allAvailabilty, error: null})
+
+        const data = ""
+
+        const newPatient = fetch(``, {
+            method: "GET",
+            body: JSON.stringify(data)
+        })
+        
+        return Response.json({user: "Filler Response" , error: null})
     }catch (error){
         return Response.json({error: error, user: null})
     }
