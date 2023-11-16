6b3aa1912fa124645c600621076ea6663ba1ad9e
odavid <odavid@uvic.ca>
2023-11-16 11:07:02 -0800

    Added incomplete routes for Patient CRUD Operations
    Changed folder name to mp-microservice

---
+++ mp-microservice/src/app/interfaces/types.ts
@@ -0,0 +1,20 @@
+/**
+ * Date: DD/MM/YYYY
+ */
+export type Date = `${number}/${number}/${number}`
+
+/**
+ * Time: HH:MM in 24 hour time
+ */
+export type Time = `${number|''}${number}:${number}${number}`
+
+/**
+ * Schedule
+ */
+export interface Schedule {
+    doctor: number
+    hoursStart: Time
+    hoursEnd: Time
+    days: String[]
+    holidays?: Date[]
+}
