07a583549ded8bf1128ddf620197211594f878db
Sydney Macdonald <macdonaldpsydney@gmail.com>
2023-11-21 16:19:48 -0800

    preliminary useState functionality

--- ehr-ui/context/AuthContext.tsx
+++ ehr-ui/context/AuthContext.tsx
@@ -5,6 +5,7 @@ import { createContext, useState, SyntheticEvent, useContext } from "react";
 
 type ContextProps = {
     user: User | null;
+    doctor: Doctor | null;
     loginUser: (e: SyntheticEvent) => void;
     logoutUser: (e: SyntheticEvent) => void;
     registerUser: (e: SyntheticEvent) => void;
@@ -17,9 +18,14 @@ type User = {
     phn: string;
 };
 
+type Doctor = {
+    id: number;
+}
+
 const AuthContext = createContext<ContextProps | null>(null);
 
 const apiURL: string = "https://log-in-microservice.vercel.app/api";
+const doctorApiURL: string = "";
 
 export default function AuthProvider({
     children,
@@ -28,6 +34,7 @@ export default function AuthProvider({
 }) {
     const router = useRouter();
     const [user, setUser] = useState<User | null>(null);
+    const [doctor, setDoctor] = useState<Doctor | null>(null);
 
     let loginUser = async (e: SyntheticEvent) => {
         e.preventDefault();
@@ -102,17 +109,46 @@ export default function AuthProvider({
 
     let loginDoctor = async (e: SyntheticEvent) => {
         e.preventDefault();
-        console.log("Doctor logged in");
+
+        const target = e.target as typeof e.target & {
+            email: { value: string };
+            password: { value: string };
+        };
+
+        let response = await fetch(`${doctorApiURL}/login`, {
+            method: "POST",
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: target.email.value,
+                password: target.password.value,
+            }),
+        });
+
+        if (response.ok) {
+            let doctorData = await response.json();
+            console.log(doctorData);
+
+            const updatedDoctor = { id: doctorData.id };
+            setDoctor(updatedDoctor);
+
+            console.log(doctor);
+            console.log("Doctor logged in");
+            router.push("/doctor/home");
+        } else {
+            console.log("Login failed.");
+        }
     }
 
     let logoutDoctor = (e: SyntheticEvent) => {
         e.preventDefault();
 
-        router.push("/doctor-login");
+        router.push("/doctor/login");
     }
 
     let contextData = {
         user: user,
+        doctor: doctor,
         loginUser: loginUser,
         logoutUser: logoutUser,
         registerUser: registerUser,
--- ehr-ui/src/app/doctor/home/page.tsx
+++ ehr-ui/src/app/doctor/home/page.tsx
@@ -1,7 +1,13 @@
+import { useAuth } from "../../../../context/AuthContext";
+
 export default function Home() {
+    let { doctor } = useAuth();
+
     return (
         <>
             <h1>This is the Medical Professional home page</h1>
+            {!doctor && <p>You are not logged in</p>}
+            {doctor && <p>ID: {doctor.id}</p>}
         </>
     );
 }
