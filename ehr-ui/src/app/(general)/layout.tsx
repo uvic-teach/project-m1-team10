30bf2a9568207468251df524615e75168b8e2f56
Jack Klobchar <jackklobchar@gmail.com>
2023-12-05 19:30:12 -0800

    add root layout to fix user state

--- ehr-ui/src/app/(general)/layout.tsx
+++ ehr-ui/src/app/(general)/layout.tsx
@@ -1,25 +1,22 @@
 "use client";
 
 import "../globals.css";
-import * as React from 'react';
+import * as React from "react";
 import { Providers } from "../providers";
 
 const metadata = {
-    title: 'Patient Webpages',
-    description: 'Patient Webpges'
-}
+    title: "Patient Webpages",
+    description: "Patient Webpges",
+};
 
-export default function RootLayout({ children,}: {children: React.ReactNode }) {
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
-        <Providers>
-            <html lang="en">
-                <body className="bg-custom-blue">
-
-                    <div className="w-screen h-screen flex place-content-center justify-center items-center">
-                        {children}
-                    </div>
-                </body>
-            </html>   
-        </Providers>
+        <div className="w-screen h-screen flex place-content-center justify-center items-center">
+            {children}
+        </div>
     );
 }
--- ehr-ui/src/app/(patient)/layout.tsx
+++ ehr-ui/src/app/(patient)/layout.tsx
@@ -5,30 +5,26 @@ import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 import { Providers } from "../providers";
 import TestNavBar from "@/components/test-navbar";
-import * as React from 'react';
+import * as React from "react";
 import { patientNavItems } from "./patientNavItems";
 
 const metadata = {
-    title: 'Patient Webpages',
-    description: 'Patient Webpges'
-}
+    title: "Patient Webpages",
+    description: "Patient Webpges",
+};
 
-export default function RootLayout({ children,}: {children: React.ReactNode }) {
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
-        <Providers>
-            <html lang="en">
-                <body className="bg-custom-blue">
-                    <TestNavBar />
-
-                    <div className="flex flex-col md:flex-row flex-1">
-                        <Sidebar SideBarItems={patientNavItems} />
-                        <main className="flex-1 m-4">
-                            {children}
-                        </main>
-                    </div>
-                </body>
-            </html>
-    </Providers>
-         
+        <>
+            <TestNavBar />
+            <div className="flex flex-col md:flex-row flex-1">
+                <Sidebar SideBarItems={patientNavItems} />
+                <main className="flex-1 m-4">{children}</main>
+            </div>
+        </>
     );
 }
