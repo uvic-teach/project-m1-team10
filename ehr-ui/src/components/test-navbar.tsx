4e51ed068a5a912e4b0db75d4f4a5c2a3f040f4f
Jack Klobchar <jackklobchar@gmail.com>
2023-12-06 12:12:02 -0800

    fix navbar

--- ehr-ui/src/components/test-navbar.tsx
+++ ehr-ui/src/components/test-navbar.tsx
@@ -1,51 +1,51 @@
-
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { CgProfile } from "react-icons/cg";
 import { IconContext } from "react-icons";
-import Link from 'next/link';
+import Link from "next/link";
 
-const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-]
-
+const navigation = [{ name: "Dashboard", href: "#", current: true }];
 
 function classNames(...classes: any) {
     return classes.filter(Boolean).join(" ");
 }
 
-export default function TestNavBar({home}:any) {
+export default function TestNavBar({ home }: any) {
     return (
+        <Disclosure as="nav" className="bg-custom-blue">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-8xl px-2 sm:px-3 lg:px-4">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">
+                                        Open main menu
+                                    </span>
+                                    {open ? (
+                                        <XMarkIcon
+                                            className="block h-6 w-6"
+                                            aria-hidden="true"
+                                        />
+                                    ) : (
+                                        <Bars3Icon
+                                            className="block h-6 w-6"
+                                            aria-hidden="true"
+                                        />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
 
-    <Disclosure as="nav" className="bg-custom-blue">
-        {({ open }) => (
-        <>
-        <div className="mx-auto max-w-8xl px-2 sm:px-3 lg:px-4">
-            <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    {/* Mobile menu button*/}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                    </Disclosure.Button>
-                </div>
-
-                <div className="place-content-start flex flex-1 sm:items-stretch justify-normal">
-                    <div className="flex flex-shrink-0">
-                        <p className=" text-4xl font-['Gigi'] text-white">
-                            <Link href={`/${home}`} >
-                            Cedule
-                            </Link>
-                        </p>
-                    </div>
-
-                </div>
+                            <div className="place-content-start flex flex-1 sm:items-stretch justify-normal">
+                                <div className="flex flex-shrink-0">
+                                    <p className=" text-4xl font-['Gigi'] text-white">
+                                        <Link href={`/${home}`}>Cedule</Link>
+                                    </p>
+                                </div>
+                            </div>
 
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <button
@@ -62,32 +62,29 @@ export default function TestNavBar({home}:any) {
                                     />
                                 </button>
 
+                                {/* Profile dropdown */}
+                                <Menu as="div" className="relative ml-3">
+                                    <div>
+                                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                            <span className="absolute -inset-1.5" />
+                                            <span className="sr-only">
+                                                Open user menu
+                                            </span>
 
-                    {/* Profile dropdown */}
-                    <Menu as="div" className="relative ml-3">
-                        <div>
-                            <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">Open user menu</span>
-                               
-                                <CgProfile className="h-8 w-8 rounded-full fill-white" color="white"/>
-                            </Menu.Button>
-                        </div>
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                        >
-                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <Menu.Item>
-                                    {({ active }) => (
-                                    <a
-                                        href="#"
-
+                                            <CgProfile
+                                                className="h-8 w-8 rounded-full fill-white"
+                                                color="white"
+                                            />
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
                                     >
                                         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                             <Menu.Item>
