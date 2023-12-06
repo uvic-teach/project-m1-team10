fb5ccfdcb560feca2d30d8dea313f79dabebb658
Jack Klobchar <jackklobchar@gmail.com>
2023-12-06 13:21:04 -0800

    correct hardcoded user dashboard

--- ehr-ui/src/app/(patient)/patient-dashboard/page.tsx
+++ ehr-ui/src/app/(patient)/patient-dashboard/page.tsx
@@ -9,58 +9,38 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
     let { user } = useAuth();
 
-    const numUpcoming:number = 6
+    const numUpcoming: number = 6;
 
     return (
         <>
-            <h1>This is the home page</h1>
-            {!user && <p>You are not logged in</p>}
-            {user && 
-                <p>Name: {user.name}</p>
-            }
-
-            {user && 
-                <p>PHN: {user.phn}</p>
-            }
-
             {/* code below should show when user is logged in */}
-            { !user &&
+            {user && (
                 <div className="flex h-full gap-12 flex-col items-center place-content-start font-['Inter']">
-
                     <div>
-                        <h1 className="text-4xl mb-5 text-white font-['Jomolhari']">Welcome {}</h1>
+                        <h1 className="text-4xl mb-5 text-white font-['Jomolhari']">
+                            Welcome, {user.name}
+                        </h1>
                     </div>
-
                     <div className="bg-custom-button-green rounded-xl p-3 left-0 hover:bg-custom-button-green/70">
                         <button className="text-white inline-flex">
-                            <FaPlus className="-ml-0.5 mr-1.5 h-5 w-5"/> Book an appointment
+                            <FaPlus className="-ml-0.5 mr-1.5 h-5 w-5" /> Book
+                            an appointment
                         </button>
                     </div>
-                
-    
-                    <div className="max-w-md mx-auto w-full md:w-1/3 bg-white rounded-xl hover:bg-custom-hover-grey shadow-md overflow-hidden md:max-w-2xl divide-x grid grid-col-3 grid-flow-col">
 
+                    <div className="max-w-md mx-auto w-full md:w-1/3 bg-white rounded-xl hover:bg-custom-hover-grey shadow-md overflow-hidden md:max-w-2xl divide-x grid grid-col-3 grid-flow-col">
                         <div className="col-span-1 ">
                             <button className="p-6">
                                 View Upcoming Appointments
                             </button>
                         </div>
-        
+
                         <div className="justify-center items-center col-span-1 p-6">
-                            <button>
-                                {numUpcoming}
-                            </button>
+                            <button>{numUpcoming}</button>
                         </div>
-        
                     </div>
-    
-                
-    
                 </div>
-            }
-
-
-
+            )}
         </>
     );
 }
