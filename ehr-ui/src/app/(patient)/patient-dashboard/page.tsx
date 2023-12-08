a2f14447d28bc610813efc03601189861c842596
Jack Klobchar <jackklobchar@gmail.com>
2023-12-07 16:07:10 -0800

    link to appointment page from dashboard

--- ehr-ui/src/app/(patient)/patient-dashboard/page.tsx
+++ ehr-ui/src/app/(patient)/patient-dashboard/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { FaPlus } from "react-icons/fa";
 import { useAppointment } from "../../../../context/AppointmentContext";
 import AppointmentCreate from "@/components/appointmentCreator";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,26 +17,25 @@ export default function Home() {
         <>
             {/* code below should show when user is logged in */}
             {user && (
-                <div className="flex h-full gap-12 flex-col items-center place-content-start font-['Inter']">
+                <div className="flex h-full gap-6 flex-col items-center place-content-start font-['Inter']">
                     <div>
-                        <h1 className="text-4xl mb-5 text-white font-['Jomolhari']">
+                        <h1 className="text-4xl mb-4 text-white font-['Jomolhari']">
                             Welcome, {user.name}
                         </h1>
                     </div>
                     <AppointmentCreate />
 
                     {appointments && (
-                        <div className="max-w-md mx-auto w-full md:w-1/3 bg-white rounded-xl hover:bg-custom-hover-grey shadow-md overflow-hidden md:max-w-2xl divide-x grid grid-col-3 grid-flow-col">
-                            <div className="col-span-1 ">
-                                <button className="p-6">
+                        <Link href="/appointments">
+                            <div className=" bg-white rounded-lg hover:bg-custom-hover-grey shadow-md divide-x flex">
+                                <button className="px-36 py-4">
                                     View Upcoming Appointments
                                 </button>
+                                <button className="px-12">
+                                    {appointments.length}
+                                </button>
                             </div>
-
-                            <div className="justify-center items-center col-span-1 p-6">
-                                <button>{appointments.length}</button>
-                            </div>
-                        </div>
+                        </Link>
                     )}
                 </div>
             )}
