eb5ec4297f767e0b24b2e24f22c5b8e132c38db2
Jack Klobchar <jackklobchar@gmail.com>
2023-12-08 22:25:31 -0800

    fix wrong link doctor sidebar

--- ehr-ui/src/app/(doctor)/doctorNavItems.tsx
+++ ehr-ui/src/app/(doctor)/doctorNavItems.tsx
@@ -4,29 +4,29 @@ import { HiUserGroup } from "react-icons/hi2";
 import { FaRegCircleCheck } from "react-icons/fa6";
 import Link from "next/link";
 
-export const doctorNavItems= [
+export const doctorNavItems = [
     {
-      name: "Dashboard",
-      href: "/doctor-dashboard",
-      icon: <MdSpaceDashboard className="w-6 h-6" />,
-      current: true
+        name: "Dashboard",
+        href: "/doctor-dashboard",
+        icon: <MdSpaceDashboard className="w-6 h-6" />,
+        current: true,
     },
     {
-      name: "Patient Lookup",
-      href: "/patient-lookup",
-      icon: <HiUserGroup className="w-6 h-6" />,
-      current: false,
+        name: "Patient Lookup",
+        href: "/patient-lookup",
+        icon: <HiUserGroup className="w-6 h-6" />,
+        current: false,
     },
     {
-      name: "Appointments",
-      href: "/appointments",
-      icon: <IoMdCalendar className="w-6 h-6" />,
-      current: false,
+        name: "Appointments",
+        href: "/appointments",
+        icon: <IoMdCalendar className="w-6 h-6" />,
+        current: false,
     },
     {
         name: "Availability",
-        href: "/availabilty",
+        href: "/availability",
         icon: <FaRegCircleCheck className="w-6 h-6" />,
-        current: false
-      },
-]
+        current: false,
+    },
+];
