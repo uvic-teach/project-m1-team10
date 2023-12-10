f317c1665ca90153064c22354a34ef36886eef39
odavid <odavid@uvic.ca>
2023-12-09 20:19:54 -0800

    modified create user route to not export hashPassword method

--- mp-microservice/src/app/api/createuser/[...params]/route.ts
+++ mp-microservice/src/app/api/createuser/[...params]/route.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { SHA256 as sha256 } from "crypto-js";
 
 //export const runtime = 'edge';
-export const hashPassword = (str: string) => {
+const hashPassword = (str: string) => {
     return sha256(str).toString();
 };
 
