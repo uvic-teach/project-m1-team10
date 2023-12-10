ea8f08a141377a5413342ada5027608816b454b4
odavid <odavid@uvic.ca>
2023-12-09 20:07:45 -0800

    Fixed Patient History CRUD Calls

--- mp-microservice/src/app/api/patient/read/[...params]/route.ts
+++ mp-microservice/src/app/api/patient/read/[...params]/route.ts
@@ -9,7 +9,7 @@ export async function GET(req:NextApiRequest, context: { params:any }) {
             const patientPHN = parseInt(context.params.params[0])
 
 
-            const response = await fetch(`https://log-in-microservice.vercel.app/api/history/?phn=${patientPHN}`, {
+            const response = await fetch(`https://log-in-microservice.vercel.app/api/history?phn=${patientPHN}`, {
                 method: "GET"
             })
 
